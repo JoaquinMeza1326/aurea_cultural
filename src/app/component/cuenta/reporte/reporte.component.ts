71ce03f1c267a418db37d01b4c2e4efc0718f7c4
xluis02 <luis.g.e0802@gmail.com>
2025-06-30 17:43:30 -0500

    Corregido reporte.ts

--- src/app/component/cuenta/reporte/reporte.component.ts
+++ src/app/component/cuenta/reporte/reporte.component.ts
@@ -1,4 +1,15 @@
-
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MenuAdminComponent } from '../../shared/menu-admin/menu-admin.component';
+import { Claim } from '../../../models/claim';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ClaimService } from '../../../services/claim.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reporte',
