7c46b0f3f0a6f88a2403b71286ed26288a01d404
xluis02 <luis.g.e0802@gmail.com>
2025-07-02 13:14:34 -0500

    Cambios eventService.ts

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EventMF } from '../models/event';
 import { environment } from '../environments/environment';
-
+// Cambios en el service
 @Injectable({
   providedIn: 'root',
 })
